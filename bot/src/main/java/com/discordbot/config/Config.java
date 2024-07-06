9eeea96ad086e5e9a4898df777dbd80ce8a5dfbc
Solange Villa <90793046+NahomiVilla@users.noreply.github.com>
2024-07-06 17:25:43 -0500

    Update Config.java

--- bot/src/main/java/com/discordbot/config/Config.java
+++ bot/src/main/java/com/discordbot/config/Config.java
@@ -1,5 +1,5 @@
 package com.discordbot.config;
 
 public class Config {
-    public static final String BOT_TOKEN = "0b26461f92e0d1b8e7b1474dc15bc0e753ecc5c8c004ee95b5b43d1e18716b81";
+    public static final String BOT_TOKEN = "TOKEN_DISCORD";
 }
